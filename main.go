91e69eac08b6d0e662538a7105019ea52944abf2
Sol Kuczala <sol.kuczala@gmail.com>
2024-04-21 19:07:01 +0100

    add logger logrus and replace logs

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"log"
+
 	"os"
 	"path/filepath"
 	"strings"
 
-	// TODO: fix broken import
 	"clialgotool/customAlgo"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	// set logrus as the default logger
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+}
 
 func getColumnIndex(header []string, columnName string) (int, error) {
 	for i, col := range header {
@@ -29,10 +36,9 @@ func main() {
 	outputFile := flag.String("output", "", "Output CSV file")
 	flag.Parse()
 
-	// TODO, make a proper cli, that doesn't return when encountering errors
 	// Check if input file is provided
 	if *inputFile == "" {
-		log.Println("Please provide input CSV file using -input flag")
+		log.Fatalf("Please provide input CSV file using -input flag")
 		return
 	}
 	// Set the default output filename if not provided
@@ -44,7 +50,7 @@ func main() {
 	// Read input CSV file
 	openInputFile, err := os.Open(*inputFile)
 	if err != nil {
-		log.Println("OS open failed: ", err)
+		log.Fatalf("OS open failed:%v", err)
 		return
 	}
 	defer openInputFile.Close()
@@ -53,31 +59,32 @@ func main() {
 	reader := csv.NewReader(openInputFile)
 	records, err := reader.ReadAll()
 	if err != nil {
-		log.Println("Reader failed: ", err)
+		log.Fatalf("Reader failed:%v", err)
 		return
 	}
 
 	columnIdx, err := getColumnIndex(records[0], "pick_location")
 	if err != nil {
+		log.Fatalf("getColumnIndex failed:%v", err)
 		return
 	}
+
 	// call module that processes CSV
 	customAlgo.OrderCSVbyColumnIdx(columnIdx, records[1:])
 
 	// Create the output CSV file
 	newCSV, err := os.Create(*outputFile)
 	if err != nil {
-		log.Println("OS create failed: ", err)
+		log.Fatalf("OS create failed:%v", err)
 		return
 	}
 	defer newCSV.Close()
 
 	// Write the processed records to the output CSV file
 	writer := csv.NewWriter(newCSV)
-	// TODO: format it correctly before writing
 	err = writer.WriteAll(records)
 	if err != nil {
-		log.Println("Writer failed: ", err)
+		log.Fatalf("Writer failed:%v", err)
 		return
 	}
 }
