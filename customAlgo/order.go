22bc56d52dddf5bbef8e718d0ac6bd46687e8cbb
Sol Kuczala <sol.kuczala@gmail.com>
2024-04-21 14:37:15 +0100

    add quicksort algo

--- customAlgo/order.go
+++ customAlgo/order.go
@@ -1,5 +1,36 @@
 package customalgo
 
 func Order([][]string) {
+	// TODO : how to apply quicksort to a matrix?
 
+}
+
+func swap(arr []int, idx1, idx2 int) {
+	temp := arr[idx1]
+	arr[idx1] = arr[idx2]
+	arr[idx2] = temp
+}
+
+func quicksort(arr []int, lo, hi int) {
+	if lo >= hi {
+		return
+	}
+	pivot := arr[hi]
+	idx := lo
+	for i := lo; i < hi; i++ {
+		if arr[i] < pivot && i != lo {
+			swap(arr, i, idx)
+			idx++
+		}
+		if arr[i] < pivot && i == lo {
+			idx++
+		}
+
+	}
+	if arr[idx] > arr[hi] {
+		swap(arr, hi, idx)
+	}
+
+	quicksort(arr, lo, idx-1)
+	quicksort(arr, idx+1, hi)
 }
